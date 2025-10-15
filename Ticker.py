841dcb02b5498a8d0d36b063bc518301ed88b585
EmiSer012 <emilien@seriot.ch>
2025-10-15 17:14:23 +0200

    change for a ascii table

--- Ticker.py
+++ Ticker.py
@@ -2,28 +2,28 @@ from PIL import Image
 import pygame
 pygame.init
 
-CHAR_WIDTH = 4
-CHAR_HEIGHT = 5
+CHAR_WIDTH = 7
+CHAR_HEIGHT = 9
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (150, 150, 150)
 RED = (255, 0, 0)
 
-S_PIXEL = 30
+S_PIXEL = 10
 MARGIN = 1
 
-W_SCREEN = 30
-H_SCREEN = 5
+W_SCREEN = 70
+H_SCREEN = 1*CHAR_HEIGHT
 
 screen = pygame.display.set_mode((W_SCREEN * S_PIXEL, H_SCREEN * S_PIXEL))
 
-img = Image.open('/Users/piniuf300iq/Desktop/font.png')
+char = Image.open('/Users/piniuf300iq/Desktop/char.png')
 
 l = []
-for s in range(CHAR_HEIGHT):
-    for r in range(CHAR_WIDTH):
-        c = (img.getpixel((r+2*CHAR_WIDTH, s+0*CHAR_HEIGHT))[0])//255
+for h in range(CHAR_HEIGHT):
+    for w in range(CHAR_WIDTH):
+        c = (char.getpixel((w+3*CHAR_WIDTH, h+2*CHAR_HEIGHT))[0])//255
         l.append(c)
 
 while True:
@@ -32,11 +32,12 @@ while True:
     screen.fill(GREY)
 
     for i,q in enumerate(l):
-        if q == 1:
+        if q == 0:
             color = GREY
-        elif q == 0:
+        elif q == 1:
             color = RED
-        pygame.draw.rect(screen, color, pygame.Rect(i%4*S_PIXEL, i//4*S_PIXEL, S_PIXEL, S_PIXEL))
+        print(q, i%7, i//9)
+        pygame.draw.rect(screen, color, pygame.Rect(i%CHAR_WIDTH*S_PIXEL, i//CHAR_WIDTH*S_PIXEL, S_PIXEL, S_PIXEL))
 
     for i in range(H_SCREEN):
         for j in range(W_SCREEN):
